b282247d8bbe66f3fd25290b8cb9d6baa17b3d68
Koskinen Veeti Gunnar <veeti.koskinen@edu.tampere.fi>
2021-10-14 11:19:34 +0300

    First push

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+    public float moveInput;
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    public int extraJumps;
+    private int extraJumpValue = 2;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        // oikea nuoli = 1, vasen nuoli = -1
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (!facingRight && moveInput > 0)
+        {
+            // jos ei katsota oikeaan ja painettu oikealle
+            Flip();
+        }
+        else if (facingRight && moveInput < 0)
+        {
+            // tai jos katsotaan oikealle ja painettu vasemmalle
+            Flip();
+        }
+    }
+}
